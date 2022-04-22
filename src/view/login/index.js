1b52144809649f1ee42a8d30676fdd0f51287a80
Davi <davi.silva@novaandradina.org>
2022-04-22 13:54:12 -0400

    autenticando usuarios

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
 
+import firebase from '../../config/firebase';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 function Login() {
+
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+    const auth = getAuth();
+
+    function logar() {
+        signInWithEmailAndPassword(auth, email, senha)
+            .then((userCredential) => {
+                // Signed in
+                alert("Conectado!")
+                // ...
+            })
+            .catch((error) => {
+                alert("Erro!")
+            });
+    }
+
     return (
         <form>
             <div className="login-content d-flex align-items-center">
@@ -11,11 +31,11 @@ function Login() {
                         <h1 className="h3 mb-3 font-weight-normal text-white font-weight-bold">Acesso Administrativo</h1>
                     </div>
 
-                    <input type="email" id="inputEmail" class="form-control my-2" placeholder="Email" />
-                    <input type="password" id="inputPassword" class="form-control my-2" placeholder="Senha" />
+                    <input onChange={(e) => setEmail(e.target.value)} type="email" id="inputEmail" class="form-control my-2" placeholder="Email" />
+                    <input onChange={(e) => setSenha(e.target.value)} type="password" id="inputPassword" class="form-control my-2" placeholder="Senha" />
 
 
-                    <button class="btn btn-lg btn-block btn-login" type="button">Acessar</button>
+                    <button onClick={logar} class="btn btn-lg btn-block btn-login" type="button">Acessar</button>
                 </form>
             </div>
         </form>
